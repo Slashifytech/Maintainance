5e2a7a3bf7f3938cf380102b8dc4930afc978cfc
Minhaz Ashraf <minhazashraf590@gmail.com>
2024-12-17 19:36:04 +0530

    chnges

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import App from './App';
 import './index.css'
-import { store } from './features/store.js'
-import { Provider } from "react-redux";
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-
+    <BrowserRouter>
     <App />
-   </Provider>
-
+    </BrowserRouter>
 )
--- src/pages/Maintanance.jsx
+++ src/pages/Maintanance.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React from "react";
+import { logo, sov } from "../assets";
 
 const Maintanance = () => {
   return (
-<>
-    
-</>  )
-}
+    <>
+      <header>
+        <img src={logo} alt="logo" className="w-40 h-24" />
+      </header>
+      <div className="font-poppins flex flex-col items-center  md:mt-9 sm:mt-20">
+  <p className="text-[38px] text-sidebar font-semibold text-center">SOV Portal is currently under maintenance.</p>
+  <p className="text-[14px] text-sidebar font-medium sm:mt-6">We apologize for any inconveniences caused. We’ve almost done.</p>
+  <img src={sov}  alt="img" />
+  
+  </div>
 
-export default Maintanance
+    </>
+  );
+};
+
+export default Maintanance;
