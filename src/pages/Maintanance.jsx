d8331b453f5b3879542193fdcbaeac54c5a94891
Minhaz Ashraf <minhazashraf590@gmail.com>
2025-07-02 10:27:59 +0530

    added code for connecting soulmate maintainance

--- src/pages/Maintanance.jsx
+++ src/pages/Maintanance.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import { logo, sov } from "../assets";
+import { logo, logo2, sov } from "../assets";
 
 const Maintanance = () => {
   return (
     <>
       <header>
-        <img src={logo} alt="logo" className="w-40 h-24" />
+        <img src={logo2} alt="logo" className="w-40 h-24" />
       </header>
       <div className="font-poppins flex flex-col items-center  md:mt-9 sm:mt-20">
-  <p className="text-[38px] text-sidebar font-semibold text-center">SOV Portal is currently under maintenance.</p>
+  <p className="text-[38px] text-sidebar font-semibold text-center">Connecting Soulmate is currently under maintenance.</p>
   <p className="text-[14px] text-sidebar font-medium sm:mt-6">We apologize for any inconvenience caused. We're almost done.</p>
   <img src={sov}  alt="img" />
   
